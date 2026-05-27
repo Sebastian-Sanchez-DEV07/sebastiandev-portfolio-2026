7d86946424f6def33e92096b620a0adf9e42f1ba
Sebastians <sebastiansdev72@gmail.com>
2026-05-26 19:06:27 -0600

    Initial default files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/Hero.tsx";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Hero />
+        </>
+    )
+}
+export default App
