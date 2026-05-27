2d522b5b8de91cef59416a4a143c2aea3462c72e
Sebastians <sebastiansdev72@gmail.com>
2026-05-26 20:09:02 -0600

    Hero section text and hero logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Hero from "./sections/Hero.tsx";
 const App = () => {
     return (
         <>
-            <h1>Hello World</h1>
             <Hero />
         </>
     )
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <section>Hero section</section>
+        <section id="hero" className="relative overflow-hidden">
+            <div className="absolute to-0 left-0 z-10">
+
+            </div>
+
+            <div className="hero-layout">
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                            <h1>Section of the hero</h1>
+                            <h1>Hello World</h1>
+                            <h1>Part 1</h1>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        </section>
     )
 }
 export default Hero
